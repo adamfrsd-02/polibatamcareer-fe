d3cae76c5c5cf16413c69114179486e1ccb67881
Kinzuka <putraantowi11@gmail.com>
2022-02-08 09:11:04 +0700

    pisahin tiap role 1 folder

---
+++ company/pages/tables.php
@@ -0,0 +1,72 @@
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+      <div class="d-block mb-4 mb-md-0">
+        <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+          <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item">
+              <a href="#">
+                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                  </path>
+                </svg>
+              </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#">Polibatam Career</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Applicant</li>
+          </ol>
+        </nav>
+        <h2 class="h4">Daftar Perusahaan</h2>
+        <!-- <p class="mb-0".</p> -->
+      </div>
+    </div>
+
+
+
+    <div class="card border-0 shadow mb-4 mt-4">
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-centered table-nowrap mb-0 rounded">
+            <thead class="thead-light">
+              <tr>
+                <th class="border-0 rounded-start">#</th>
+                <th class="border-0">Nama Perusahaan</th>
+                <th class="border-0">Alamat</th>
+                <th class="border-0">Status MoU</th>
+                <th class="border-0">No. Telepon</th>
+              </tr>
+            </thead>
+            <tbody>
+              <!-- Item -->
+              <tr>
+                <td><a href="#" class="text-primary fw-bold">1</a> </td>
+                <td class="fw-bold d-flex align-items-center">
+                  <svg class="icon icon-xxs text-gray-500 me-2" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+                      clip-rule="evenodd"></path>
+                  </svg>
+                  PT. Phillips Indonesia
+                </td>
+                <td>
+                  Batam Center
+                </td>
+                <td>
+                  Ok
+                </td>
+                <td>
+                  +1 2989
+                </td>
+              </tr>
+              <!-- End of Item -->
+
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+
+
+
---
+++ users/pages/transactions.php
@@ -0,0 +1,72 @@
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
+      <div class="d-block mb-4 mb-md-0">
+        <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+          <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item">
+              <a href="#">
+                <svg class="icon icon-xxs" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
+                  </path>
+                </svg>
+              </a>
+            </li>
+            <li class="breadcrumb-item"><a href="#">Polibatam Career</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Applicant</li>
+          </ol>
+        </nav>
+        <h2 class="h4">Daftar Applicant</h2>
+        <!-- <p class="mb-0".</p> -->
+      </div>
+    </div>
+
+    <div class="card card-body border-0 shadow table-wrapper table-responsive">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th class="border-gray-200">#</th>
+            <th class="border-gray-200">Username</th>
+            <th class="border-gray-200">Nama Applicant</th>
+            <th class="border-gray-200">NIM</th>
+            <th class="border-gray-200">Pekerjaan</th>
+            <th class="border-gray-200">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Item -->
+          <tr>
+            <td>
+              <a href="#" class="fw-bold">
+                1
+              </a>
+            </td>
+            <td>
+              <span class="fw-normal">adam.4311901038</span>
+            </td>
+            <td><span class="fw-normal">Adam Firdaus</span></td>
+            <td><span class="fw-normal">4311901038</span></td>
+            <td><span class="fw-bold text-warning">Freelancer</span></td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-link text-dark dropdown-toggle dropdown-toggle-split m-0 p-0"
+                  data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="icon icon-sm">
+                    <span class="fas fa-ellipsis-h icon-dark"></span>
+                  </span>
+                  <span class="visually-hidden">Toggle Dropdown</span>
+                </button>
+                <div class="dropdown-menu py-0">
+                  <a class="dropdown-item rounded-top" href="#"><span class="fas fa-eye me-2"></span>View Details</a>
+                  <a class="dropdown-item" href="#"><span class="fas fa-edit me-2"></span>Edit</a>
+                  <a class="dropdown-item text-danger rounded-bottom" href="#"><span
+                      class="fas fa-trash-alt me-2"></span>Remove</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+
+    </div>
