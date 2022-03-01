b020af139f226fe38f11a0f89f139d3baed8b1d6
Kinzuka <putraantowi11@gmail.com>
2022-03-01 19:45:05 +0700

    Create db_jobportal (1).sql

---
+++ database/db_jobportal (1).sql
@@ -0,0 +1,462 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Mar 2022 pada 13.28
+-- Versi server: 10.4.17-MariaDB
+-- Versi PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_jobportal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblapplicants`
+--
+
+CREATE TABLE `tblapplicants` (
+  `APPLICANTID` int(11) NOT NULL,
+  `FNAME` varchar(90) NOT NULL,
+  `LNAME` varchar(90) NOT NULL,
+  `MNAME` varchar(90) NOT NULL,
+  `ADDRESS` varchar(255) NOT NULL,
+  `SEX` varchar(11) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(255) NOT NULL,
+  `AGE` int(2) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `EMAILADDRESS` varchar(90) NOT NULL,
+  `CONTACTNO` varchar(90) NOT NULL,
+  `DEGREE` text NOT NULL,
+  `APPLICANTPHOTO` varchar(255) NOT NULL,
+  `NATIONALID` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblapplicants`
+--
+
+INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `SEX`, `CIVILSTATUS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `USERNAME`, `PASS`, `EMAILADDRESS`, `CONTACTNO`, `DEGREE`, `APPLICANTPHOTO`, `NATIONALID`) VALUES
+(2019016, 'asd', 'asd', 'asd', 'asd', 'Female', 'none', '1980-01-29', 'asd', 39, 'aa', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37', 'a@gmil.com', '12312312', 'asd', 'photos/final.png', ''),
+(2019018, 'asdasd', 'asd', 'asd', 'sadas', 'Female', 'Single', '1992-01-12', 'sad', 27, 'ss', 'c1c93f88d273660be5358cd4ee2df2c2f3f0e8e7', 'a@gmil.com', 'sad', 'sad', '', ''),
+(2019020, 'sad', 'sad', 'sad', 'asdsad', 'Female', 'Single', '1992-10-14', 'asdsad', 27, 'ddd', '9c969ddf454079e3d439973bbab63ea6233e4087', 'a@gmil.com', '123123', 'sadsadsad', 'photos/077db70b-ab84-46c4-bbaa-a5dd6b7332a4_200x200.png', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblattachmentfile`
+--
+
+CREATE TABLE `tblattachmentfile` (
+  `FILEID` int(11) NOT NULL,
+  `JOBID` int(11) NOT NULL,
+  `FILE_NAME` varchar(90) NOT NULL,
+  `FILE_LOCATION` varchar(255) NOT NULL,
+  `USERATTACHMENTID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblattachmentfile`
+--
+
+INSERT INTO `tblattachmentfile` (`FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOCATION`, `USERATTACHMENTID`) VALUES
+(201900001, 2, 'Resume', 'photos/24122019073209Filtering a Group of Data VB.Net and SQL Server 2019.docx', 2019020),
+(2147483647, 2, 'Resume', 'photos/24122019072801Filtering a Group of Data VB.Net and SQL Server 2019.docx', 2019019);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblautonumbers`
+--
+
+CREATE TABLE `tblautonumbers` (
+  `AUTOID` int(11) NOT NULL,
+  `AUTOSTART` varchar(30) NOT NULL,
+  `AUTOEND` int(11) NOT NULL,
+  `AUTOINC` int(11) NOT NULL,
+  `AUTOKEY` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblautonumbers`
+--
+
+INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTOKEY`) VALUES
+(1, '02983', 8, 1, 'userid'),
+(2, '000', 79, 1, 'employeeid'),
+(3, '0', 21, 1, 'APPLICANT'),
+(4, '0000', 2, 1, 'FILEID');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblcategory`
+--
+
+CREATE TABLE `tblcategory` (
+  `CATEGORYID` int(11) NOT NULL,
+  `CATEGORY` varchar(90) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblcategory`
+--
+
+INSERT INTO `tblcategory` (`CATEGORYID`, `CATEGORY`) VALUES
+(10, 'Technology'),
+(11, 'Managerial'),
+(12, 'Engineer'),
+(13, 'IT'),
+(14, 'Civil Engineer'),
+(15, 'HR'),
+(23, 'Sales'),
+(24, 'Banking'),
+(25, 'Finance'),
+(26, 'BPO'),
+(27, 'Degital Marketing'),
+(28, 'Shipping');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblcompany`
+--
+
+CREATE TABLE `tblcompany` (
+  `COMPANYID` int(11) NOT NULL,
+  `COMPANYNAME` varchar(90) NOT NULL,
+  `COMPANYADDRESS` varchar(90) NOT NULL,
+  `COMPANYCONTACTNO` varchar(30) NOT NULL,
+  `COMPANYSTATUS` varchar(90) NOT NULL,
+  `COMPANYMISSION` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblcompany`
+--
+
+INSERT INTO `tblcompany` (`COMPANYID`, `COMPANYNAME`, `COMPANYADDRESS`, `COMPANYCONTACTNO`, `COMPANYSTATUS`, `COMPANYMISSION`) VALUES
+(2, 'Lapmodo Batam', 'Batam Center', '8912994', '', 'Oke');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `INCID` int(11) NOT NULL,
+  `EMPLOYEEID` varchar(30) NOT NULL,
+  `FNAME` varchar(50) NOT NULL,
+  `LNAME` varchar(50) NOT NULL,
+  `MNAME` varchar(50) NOT NULL,
+  `ADDRESS` varchar(90) NOT NULL,
+  `BIRTHDATE` date NOT NULL,
+  `BIRTHPLACE` varchar(90) NOT NULL,
+  `AGE` int(11) NOT NULL,
+  `SEX` varchar(30) NOT NULL,
+  `CIVILSTATUS` varchar(30) NOT NULL,
+  `TELNO` varchar(40) NOT NULL,
+  `EMP_EMAILADDRESS` varchar(90) NOT NULL,
+  `CELLNO` varchar(30) NOT NULL,
+  `POSITION` varchar(50) NOT NULL,
+  `WORKSTATS` varchar(90) NOT NULL,
+  `EMPPHOTO` varchar(255) NOT NULL,
+  `EMPUSERNAME` varchar(90) NOT NULL,
+  `EMPPASSWORD` varchar(125) NOT NULL,
+  `DATEHIRED` date NOT NULL,
+  `COMPANYID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`INCID`, `EMPLOYEEID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `SEX`, `CIVILSTATUS`, `TELNO`, `EMP_EMAILADDRESS`, `CELLNO`, `POSITION`, `WORKSTATS`, `EMPPHOTO`, `EMPUSERNAME`, `EMPPASSWORD`, `DATEHIRED`, `COMPANYID`) VALUES
+(76, '2018001', 'Anto', 'Saputra', 'Akwokwoakwo', 'Bengkong', '2000-01-13', 'Pemalang', 22, 'Male', 'Married', '032656', 'antoawkoakwo@gmail.com', '', 'Programmer', '', '', '2018001', 'f3593fd40c55c33d1788309d4137e82f5eab0dea', '2018-05-23', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblfeedback`
+--
+
+CREATE TABLE `tblfeedback` (
+  `FEEDBACKID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `REGISTRATIONID` int(11) NOT NULL,
+  `FEEDBACK` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbljob`
+--
+
+CREATE TABLE `tbljob` (
+  `JOBID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `CATEGORY` varchar(90) NOT NULL,
+  `OCCUPATIONTITLE` varchar(90) NOT NULL,
+  `REQ_NO_EMPLOYEES` int(11) NOT NULL,
+  `SALARIES` double NOT NULL,
+  `DURATION_EMPLOYEMENT` varchar(90) NOT NULL,
+  `QUALIFICATION_WORKEXPERIENCE` text NOT NULL,
+  `JOBDESCRIPTION` text NOT NULL,
+  `PREFEREDSEX` varchar(30) NOT NULL,
+  `SECTOR_VACANCY` text NOT NULL,
+  `JOBSTATUS` varchar(90) NOT NULL,
+  `DATEPOSTED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbljob`
+--
+
+INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
+(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbljobregistration`
+--
+
+CREATE TABLE `tbljobregistration` (
+  `REGISTRATIONID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `JOBID` int(11) NOT NULL,
+  `APPLICANTID` int(11) NOT NULL,
+  `APPLICANT` varchar(90) NOT NULL,
+  `REGISTRATIONDATE` date NOT NULL,
+  `REMARKS` varchar(255) NOT NULL DEFAULT 'Pending',
+  `FILEID` int(11) NOT NULL,
+  `PENDINGAPPLICATION` tinyint(1) NOT NULL DEFAULT 1,
+  `HVIEW` tinyint(1) NOT NULL DEFAULT 1,
+  `DATETIMEAPPROVED` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblmahasiswa`
+--
+
+CREATE TABLE `tblmahasiswa` (
+  `STUDENTID` int(18) NOT NULL,
+  `PASSWORD` varchar(200) NOT NULL,
+  `FNAME` varchar(100) NOT NULL,
+  `LNAME` varchar(100) NOT NULL,
+  `MAJORITY` varchar(100) NOT NULL,
+  `STUDYPROGRAM` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblprogress`
+--
+
+CREATE TABLE `tblprogress` (
+  `id_progress` int(11) NOT NULL,
+  `id_company` int(11) NOT NULL,
+  `detail_progress` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblusers`
+--
+
+CREATE TABLE `tblusers` (
+  `USERID` varchar(30) NOT NULL,
+  `FULLNAME` varchar(40) NOT NULL,
+  `USERNAME` varchar(90) NOT NULL,
+  `PASS` varchar(90) NOT NULL,
+  `ROLE` varchar(30) NOT NULL,
+  `PICLOCATION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tblusers`
+--
+
+INSERT INTO `tblusers` (`USERID`, `FULLNAME`, `USERNAME`, `PASS`, `ROLE`, `PICLOCATION`) VALUES
+('00018', 'Adam Firdaus', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'Administrator', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_user_progress`
+--
+
+CREATE TABLE `tbl_user_progress` (
+  `id_user_progress` int(11) NOT NULL,
+  `id_progress` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `progres_step` int(11) NOT NULL,
+  `appliance_status` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  ADD PRIMARY KEY (`APPLICANTID`);
+
+--
+-- Indeks untuk tabel `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  ADD PRIMARY KEY (`FILEID`);
+
+--
+-- Indeks untuk tabel `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  ADD PRIMARY KEY (`AUTOID`);
+
+--
+-- Indeks untuk tabel `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  ADD PRIMARY KEY (`CATEGORYID`);
+
+--
+-- Indeks untuk tabel `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  ADD PRIMARY KEY (`COMPANYID`);
+
+--
+-- Indeks untuk tabel `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`INCID`),
+  ADD UNIQUE KEY `EMPLOYEEID` (`EMPLOYEEID`);
+
+--
+-- Indeks untuk tabel `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  ADD PRIMARY KEY (`FEEDBACKID`);
+
+--
+-- Indeks untuk tabel `tbljob`
+--
+ALTER TABLE `tbljob`
+  ADD PRIMARY KEY (`JOBID`);
+
+--
+-- Indeks untuk tabel `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  ADD PRIMARY KEY (`REGISTRATIONID`);
+
+--
+-- Indeks untuk tabel `tblmahasiswa`
+--
+ALTER TABLE `tblmahasiswa`
+  ADD PRIMARY KEY (`STUDENTID`);
+
+--
+-- Indeks untuk tabel `tblprogress`
+--
+ALTER TABLE `tblprogress`
+  ADD PRIMARY KEY (`id_progress`);
+
+--
+-- Indeks untuk tabel `tblusers`
+--
+ALTER TABLE `tblusers`
+  ADD PRIMARY KEY (`USERID`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tblapplicants`
+--
+ALTER TABLE `tblapplicants`
+  MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2019021;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblattachmentfile`
+--
+ALTER TABLE `tblattachmentfile`
+  MODIFY `FILEID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2147483648;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblautonumbers`
+--
+ALTER TABLE `tblautonumbers`
+  MODIFY `AUTOID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblcategory`
+--
+ALTER TABLE `tblcategory`
+  MODIFY `CATEGORYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  MODIFY `COMPANYID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `INCID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblfeedback`
+--
+ALTER TABLE `tblfeedback`
+  MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbljob`
+--
+ALTER TABLE `tbljob`
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbljobregistration`
+--
+ALTER TABLE `tbljobregistration`
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblprogress`
+--
+ALTER TABLE `tblprogress`
+  MODIFY `id_progress` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
