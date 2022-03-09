9b2973949bff70879eb5b4bea2b9e0a341d7ee40
Kinzuka <putraantowi11@gmail.com>
2022-03-09 08:39:03 +0700

    Update db_jobportal.sql

--- database/db_jobportal.sql
+++ database/db_jobportal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Mar 2022 pada 13.42
+-- Waktu pembuatan: 09 Mar 2022 pada 02.38
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 8.0.7
 
@@ -53,9 +53,118 @@ CREATE TABLE `tblapplicants` (
 --
 
 INSERT INTO `tblapplicants` (`APPLICANTID`, `FNAME`, `LNAME`, `MNAME`, `ADDRESS`, `SEX`, `CIVILSTATUS`, `BIRTHDATE`, `BIRTHPLACE`, `AGE`, `USERNAME`, `PASS`, `EMAILADDRESS`, `CONTACTNO`, `DEGREE`, `APPLICANTPHOTO`, `NATIONALID`, `LINKEDINLINK`) VALUES
-(2019016, 'Anto Wijaya', 'Saputra', 'asd', 'bengkong', 'Male', 'none', '1980-01-29', 'asd', 42, 'aa', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37', 'a@gmil.com', '08882319930', 'asd', 'photos/ktp.jpeg', '', 'https://www.linkedin.com/in/anto-wijaya-saputra-b078b3163/'),
-(2019018, 'asdasd', 'asd', 'asd', 'sadas', 'Female', 'Single', '1992-01-12', 'sad', 27, 'ss', 'c1c93f88d273660be5358cd4ee2df2c2f3f0e8e7', 'a@gmil.com', 'sad', 'sad', '', '', 'https://www.linkedin.com/in/anto-wijaya-saputra-b078b3163/'),
+(2019016, 'Anto Wijaya', 'Saputra', 'asd', 'bengkong', 'Male', 'none', '1980-01-29', 'asd', 42, 'aa', 'e0c9035898dd52fc65c41454cec9c4d2611bfb37', 'a@gmil.com', '08882319930', 'asd', 'photos/08032022021325270px-LogoPSI.svg.png', '', 'https://www.linkedin.com/in/anto-wijaya-saputra-b078b3163/'),
+(2019018, 'asdasd', 'asd', 'asd', 'sadas', 'Female', 'Single', '1992-01-12', 'sad', 27, 'ss', 'c1c93f88d273660be5358cd4ee2df2c2f3f0e8e7', 'a@gmil.com', 'sad', 'sad', 'photos/08032022084331download.jfif', '', 'https://www.linkedin.com/in/anto-wijaya-saputra-b078b3163/'),
 (2019020, 'sad', 'sad', 'sad', 'asdsad', 'Female', 'Single', '1992-10-14', 'asdsad', 27, 'ddd', '9c969ddf454079e3d439973bbab63ea6233e4087', 'a@gmil.com', '123123', 'sadsadsad', 'photos/077db70b-ab84-46c4-bbaa-a5dd6b7332a4_200x200.png', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblassesment`
+--
+
+CREATE TABLE `tblassesment` (
+  `ASSESMENTID` int(30) NOT NULL,
+  `QUESTION` text NOT NULL,
+  `QID` int(30) NOT NULL,
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tblassesment`
+--
+
+INSERT INTO `tblassesment` (`ASSESMENTID`, `QUESTION`, `QID`, `date_updated`) VALUES
+(2, 'siapakah nama saya', 2, '0000-00-00 00:00:00'),
+(3, 'i don\'t know', 3, '0000-00-00 00:00:00'),
+(4, 'another test', 4, '0000-00-00 00:00:00'),
+(5, 'just testing', 4, '2022-03-09 05:27:44');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblassesmentlist`
+--
+
+CREATE TABLE `tblassesmentlist` (
+  `ASSESMENTLISTID` int(30) NOT NULL,
+  `TITLE` text NOT NULL,
+  `JOBID` int(11) NOT NULL,
+  `COMPANYID` int(11) NOT NULL,
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tblassesmentlist`
+--
+
+INSERT INTO `tblassesmentlist` (`ASSESMENTLISTID`, `TITLE`, `JOBID`, `COMPANYID`, `date_updated`) VALUES
+(2, 'another assesment', 7, 2, '2022-03-09 05:47:03'),
+(3, 'TEST ASSESMENT', 8, 2, '2022-03-08 20:18:00'),
+(4, 'second assesment', 8, 2, '2022-03-08 20:19:00');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblassesmentopt`
+--
+
+CREATE TABLE `tblassesmentopt` (
+  `ASSESMENTOPTID` int(30) NOT NULL,
+  `OPTIONTEXT` text NOT NULL,
+  `ASSESMENTID` int(30) NOT NULL,
+  `is_right` tinyint(4) NOT NULL COMMENT '1 = right answer',
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tblassesmentopt`
+--
+
+INSERT INTO `tblassesmentopt` (`ASSESMENTOPTID`, `OPTIONTEXT`, `ASSESMENTID`, `is_right`, `date_updated`) VALUES
+(1, 'asda', 0, 1, '0000-00-00 00:00:00'),
+(2, 'asda', 0, 0, '0000-00-00 00:00:00'),
+(3, 'asdaa', 0, 0, '0000-00-00 00:00:00'),
+(4, 'as', 0, 0, '0000-00-00 00:00:00'),
+(5, 'adam', 2, 1, '0000-00-00 00:00:00'),
+(6, 'firdaus', 2, 0, '0000-00-00 00:00:00'),
+(7, 'anto', 2, 0, '0000-00-00 00:00:00'),
+(8, 'wijaya', 2, 0, '0000-00-00 00:00:00'),
+(13, 'idk man', 4, 1, '0000-00-00 00:00:00'),
+(14, 'yeah', 4, 0, '0000-00-00 00:00:00'),
+(15, 'no', 4, 0, '0000-00-00 00:00:00'),
+(16, 'nope', 4, 0, '0000-00-00 00:00:00'),
+(17, 'a', 3, 0, '2022-03-09 04:21:16'),
+(18, 'b', 3, 1, '2022-03-09 04:21:16'),
+(19, 'c', 3, 0, '2022-03-09 04:21:16'),
+(20, 'd', 3, 0, '2022-03-09 04:21:16'),
+(21, '12', 5, 0, '2022-03-09 05:27:44'),
+(22, '11', 5, 0, '2022-03-09 05:27:44'),
+(23, '10', 5, 0, '2022-03-09 05:27:44'),
+(24, '9', 5, 0, '2022-03-09 05:27:44');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tblassesmentresult`
+--
+
+CREATE TABLE `tblassesmentresult` (
+  `id` int(30) NOT NULL,
+  `ASSESMENTLISTID` int(30) NOT NULL,
+  `APPLICANTID` int(30) NOT NULL,
+  `score` int(11) NOT NULL,
+  `total_score` int(11) NOT NULL,
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tblassesmentresult`
+--
+
+INSERT INTO `tblassesmentresult` (`id`, `ASSESMENTLISTID`, `APPLICANTID`, `score`, `total_score`, `date_updated`) VALUES
+(1, 3, 2019018, 1, 1, '2022-03-09 04:55:22'),
+(5, 4, 2019018, 0, 4, '2022-03-09 05:59:05');
 
 -- --------------------------------------------------------
 
@@ -78,6 +187,9 @@ CREATE TABLE `tblattachmentfile` (
 INSERT INTO `tblattachmentfile` (`FILEID`, `JOBID`, `FILE_NAME`, `FILE_LOCATION`, `USERATTACHMENTID`) VALUES
 (201900001, 2, 'Resume', 'photos/24122019073209Filtering a Group of Data VB.Net and SQL Server 2019.docx', 2019020),
 (202200002, 1, 'Resume', 'photos/07032022023905ktp.jpeg', 2019016),
+(202200006, 8, 'Resume', 'photos/08032022092400Antos_Resume.pdf', 2019018),
+(202200007, 8, 'Resume', 'photos/08032022092940Antos_Resume.pdf', 2019018),
+(202200008, 8, 'Resume', 'photos/08032022094020Antos_Resume.pdf', 2019018),
 (2147483647, 2, 'Resume', 'photos/24122019072801Filtering a Group of Data VB.Net and SQL Server 2019.docx', 2019019);
 
 -- --------------------------------------------------------
@@ -102,7 +214,7 @@ INSERT INTO `tblautonumbers` (`AUTOID`, `AUTOSTART`, `AUTOEND`, `AUTOINC`, `AUTO
 (1, '02983', 9, 1, 'userid'),
 (2, '000', 79, 1, 'employeeid'),
 (3, '0', 21, 1, 'APPLICANT'),
-(4, '0000', 3, 1, 'FILEID');
+(4, '0000', 9, 1, 'FILEID');
 
 -- --------------------------------------------------------
 
@@ -212,6 +324,20 @@ CREATE TABLE `tblfeedback` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `tblinterview`
+--
+
+CREATE TABLE `tblinterview` (
+  `INTERVIEWID` int(30) NOT NULL,
+  `id_progress` int(30) NOT NULL,
+  `INTERVIEWLINK` text NOT NULL,
+  `INTERVIEWDATE` datetime NOT NULL,
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `tbljob`
 --
 
@@ -228,15 +354,21 @@ CREATE TABLE `tbljob` (
   `PREFEREDSEX` varchar(30) NOT NULL,
   `SECTOR_VACANCY` text NOT NULL,
   `JOBSTATUS` varchar(90) NOT NULL,
-  `DATEPOSTED` datetime NOT NULL
+  `DATEPOSTED` datetime NOT NULL,
+  `PROGRESS_DETAIL` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `tbljob`
 --
 
-INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`) VALUES
-(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00');
+INSERT INTO `tbljob` (`JOBID`, `COMPANYID`, `CATEGORY`, `OCCUPATIONTITLE`, `REQ_NO_EMPLOYEES`, `SALARIES`, `DURATION_EMPLOYEMENT`, `QUALIFICATION_WORKEXPERIENCE`, `JOBDESCRIPTION`, `PREFEREDSEX`, `SECTOR_VACANCY`, `JOBSTATUS`, `DATEPOSTED`, `PROGRESS_DETAIL`) VALUES
+(1, 2, 'Technology', 'ISD', 6, 15000, 'jan 30', 'Two year Experience', 'We are looking for bachelor of science in information technology.\r\nasdasdasd', 'Male/Female', 'yes', '', '2018-05-20 00:00:00', ''),
+(4, 2, 'IT', 'WEB DESIGN', 10, 125000, '1 week', 'asdas', 'asd', 'Male/Female', 'asd', '', '2022-03-08 14:42:00', ''),
+(5, 8, 'IT', 'NETWORK', 120, 150000, '1239012039', '123123', '11231', 'Male/Female', '1231231', '', '2022-03-08 14:46:00', ''),
+(6, 2, 'Technology', 'CONTOH SAJA', 20, 150000, '123123', '123123', '123123', 'Male/Female', '232323', '', '2022-03-08 18:14:00', ''),
+(7, 2, 'Finance', 'BANK SWASTA', 20, 120312031023, '10 WEEK', 'APA AJALAH', 'ASD', 'Male/Female', 'ASD', '', '2022-03-08 18:35:00', 'a:4:{i:0;s:9:\"assesment\";i:1;s:9:\"interview\";i:2;s:9:\"interview\";i:3;s:2:\"mc\";}'),
+(8, 2, 'HR', 'just test again', 20, 125000, '1 week', 'senior on everything', 'just doing work', 'Male/Female', 'work', '', '2022-03-08 20:18:00', 'a:6:{i:0;s:9:\"assesment\";i:1;s:9:\"assesment\";i:2;s:9:\"interview\";i:3;s:9:\"interview\";i:4;s:15:\"medical checkup\";i:5;s:5:\"hired\";}\r\n');
 
 -- --------------------------------------------------------
 
@@ -263,22 +395,7 @@ CREATE TABLE `tbljobregistration` (
 --
 
 INSERT INTO `tbljobregistration` (`REGISTRATIONID`, `COMPANYID`, `JOBID`, `APPLICANTID`, `APPLICANT`, `REGISTRATIONDATE`, `REMARKS`, `FILEID`, `PENDINGAPPLICATION`, `HVIEW`, `DATETIMEAPPROVED`) VALUES
-(6, 2, 1, 2019016, 'adam firdaus', '2022-03-07', 'Success', 202200002, 0, 0, '2022-03-07 19:58:17');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tblmahasiswa`
---
-
-CREATE TABLE `tblmahasiswa` (
-  `STUDENTID` int(18) NOT NULL,
-  `PASSWORD` varchar(200) NOT NULL,
-  `FNAME` varchar(100) NOT NULL,
-  `LNAME` varchar(100) NOT NULL,
-  `MAJORITY` varchar(100) NOT NULL,
-  `STUDYPROGRAM` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(12, 2, 8, 2019018, 'asdasd asd', '2022-03-08', 'Success', 202200008, 0, 0, '2022-03-09 08:32:16');
 
 -- --------------------------------------------------------
 
@@ -291,15 +408,16 @@ CREATE TABLE `tblprogress` (
   `COMPANYID` int(11) NOT NULL,
   `JOBID` int(11) NOT NULL,
   `APPLICANTID` int(11) NOT NULL,
-  `detail_progress` text NOT NULL
+  `detail_progress` text NOT NULL,
+  `ASSESMENTID` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `tblprogress`
 --
 
-INSERT INTO `tblprogress` (`id_progress`, `COMPANYID`, `JOBID`, `APPLICANTID`, `detail_progress`) VALUES
-(1, 2, 0, 2019016, 'a:3:{i:0;s:9:\"assesment\";i:1;s:9:\"interview\";i:2;s:10:\"third step\";}');
+INSERT INTO `tblprogress` (`id_progress`, `COMPANYID`, `JOBID`, `APPLICANTID`, `detail_progress`, `ASSESMENTID`) VALUES
+(7, 2, 8, 2019018, 'a:6:{i:0;s:9:\"assesment\";i:1;s:9:\"assesment\";i:2;s:9:\"interview\";i:3;s:9:\"interview\";i:4;s:15:\"medical checkup\";i:5;s:5:\"hired\";}\r\n', '');
 
 -- --------------------------------------------------------
 
@@ -365,7 +483,7 @@ CREATE TABLE `tbl_user_progress` (
 --
 
 INSERT INTO `tbl_user_progress` (`id_user_progress`, `id_progress`, `APPLICANTID`, `progres_step`, `appliance_status`) VALUES
-(1, 1, 2019016, 2, 'pending');
+(2, 7, 2019018, 6, 'accepted');
 
 --
 -- Indexes for dumped tables
@@ -376,6 +494,30 @@ INSERT INTO `tbl_user_progress` (`id_user_progress`, `id_progress`, `APPLICANTID
 --
 ALTER TABLE `tblapplicants`
   ADD PRIMARY KEY (`APPLICANTID`);
+
+--
+-- Indeks untuk tabel `tblassesment`
+--
+ALTER TABLE `tblassesment`
+  ADD PRIMARY KEY (`ASSESMENTID`);
+
+--
+-- Indeks untuk tabel `tblassesmentlist`
+--
+ALTER TABLE `tblassesmentlist`
+  ADD PRIMARY KEY (`ASSESMENTLISTID`);
+
+--
+-- Indeks untuk tabel `tblassesmentopt`
+--
+ALTER TABLE `tblassesmentopt`
+  ADD PRIMARY KEY (`ASSESMENTOPTID`);
+
+--
+-- Indeks untuk tabel `tblassesmentresult`
+--
+ALTER TABLE `tblassesmentresult`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `tblattachmentfile`
@@ -415,6 +557,12 @@ ALTER TABLE `tblfeedback`
   ADD PRIMARY KEY (`FEEDBACKID`);
 
 --
+-- Indeks untuk tabel `tblinterview`
+--
+ALTER TABLE `tblinterview`
+  ADD PRIMARY KEY (`INTERVIEWID`);
+
+--
 -- Indeks untuk tabel `tbljob`
 --
 ALTER TABLE `tbljob`
@@ -425,12 +573,6 @@ ALTER TABLE `tbljob`
 --
 ALTER TABLE `tbljobregistration`
   ADD PRIMARY KEY (`REGISTRATIONID`);
-
---
--- Indeks untuk tabel `tblmahasiswa`
---
-ALTER TABLE `tblmahasiswa`
-  ADD PRIMARY KEY (`STUDENTID`);
 
 --
 -- Indeks untuk tabel `tblprogress`
@@ -465,6 +607,30 @@ ALTER TABLE `tbl_user_progress`
 --
 ALTER TABLE `tblapplicants`
   MODIFY `APPLICANTID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2019021;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblassesment`
+--
+ALTER TABLE `tblassesment`
+  MODIFY `ASSESMENTID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblassesmentlist`
+--
+ALTER TABLE `tblassesmentlist`
+  MODIFY `ASSESMENTLISTID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblassesmentopt`
+--
+ALTER TABLE `tblassesmentopt`
+  MODIFY `ASSESMENTOPTID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `tblassesmentresult`
+--
+ALTER TABLE `tblassesmentresult`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `tblattachmentfile`
@@ -503,28 +669,40 @@ ALTER TABLE `tblfeedback`
   MODIFY `FEEDBACKID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT untuk tabel `tblinterview`
+--
+ALTER TABLE `tblinterview`
+  MODIFY `INTERVIEWID` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT untuk tabel `tbljob`
 --
 ALTER TABLE `tbljob`
-  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `JOBID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbljobregistration`
 --
 ALTER TABLE `tbljobregistration`
-  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `REGISTRATIONID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT untuk tabel `tblprogress`
 --
 ALTER TABLE `tblprogress`
-  MODIFY `id_progress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_progress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `tbl_curriculum_vitae`
 --
 ALTER TABLE `tbl_curriculum_vitae`
   MODIFY `id_cv` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_user_progress`
+--
+ALTER TABLE `tbl_user_progress`
+  MODIFY `id_user_progress` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
